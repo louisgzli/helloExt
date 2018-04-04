af8d50eaf2b9f4df58ac9e59aac157d26284876d
xiyuan1223 <1446327445@qq.com>
2018-04-04 17:32:59 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Administrator on 2018/4/3 0003.
+ */
+Ext.application({
+    name: 'HelloExt',
+    launch: function() {
+        var childPanel1 = Ext.create('Ext.panel.Panel', {
+            title: 'Child Panel 1',
+            html: 'A Panel'
+        });
+
+        var childPanel2 = Ext.create('Ext.panel.Panel', {
+            title: 'Child Panel 2',
+            html: 'Another Panel'
+        });
+
+        Ext.create('Ext.container.Viewport', {
+            items: [ childPanel1, childPanel2 ]
+        });
+
+    }
+});
---
+++ js/panel.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Administrator on 2018/4/4 0004.
+ */
